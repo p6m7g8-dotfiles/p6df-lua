518dd4cb8479305ea68700c610dcfdfa9a40e4e2
Philip M. Gollucci <pgollucci@p6m7g8.com>
2022-07-10 21:27:20 -0400

    chore: p6df::util -> p6df::core::util

--- init.zsh
+++ init.zsh
@@ -65,7 +65,7 @@ p6df::modules::lua::luaenv::init() {
     export LUAENV_ROOT
     export HAS_LUAENV=1
 
-    p6df::util::path_if $LUAENV_ROOT/bin
+    p6_path_if $LUAENV_ROOT/bin
     eval "$(p6_run_code luaenv init - zsh)"
   fi
 }
