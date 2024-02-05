d29e7366f64abe78720e574061c06cced1dd2b5c
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-02-05 18:30:47 -0500

    fix: this is lua not node

--- init.zsh
+++ init.zsh
@@ -62,10 +62,10 @@ p6df::modules::lua::langs() {
 
   # get the shiny one
   local latest=$(p6df::modules::lua::luaenv::latest)
-  nodenv install -s $latest
+  luaenv install -s $latest
 
-  nodenv global $latest
-  nodenv rehash
+  luaenv global $latest
+  luaenv rehash
 
   p6_return_void
 }
@@ -89,7 +89,7 @@ p6df::modules::lua::luaenv::latest() {
 #
 #>
 ######################################################################
-p6df::modules::lua::luaenv::latest() {
+p6df::modules::lua::luaenv::latest::installed() {
 
   luaenv install -l | p6_filter_select "5" | p6_filter_exclude jit | p6_filter_from_end "2" | p6_filter_spaces_strip
 }
