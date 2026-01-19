4908066a3115765d44f50410b4fc8c92f037c811
Philip M. Gollucci <pgollucci@p6m7g8.com>
2026-01-18 22:15:15 -0500

    chore: converts to new prompt v4 system etc

--- init.zsh
+++ init.zsh
@@ -66,13 +66,13 @@ p6df::modules::lua::langs() {
 
   # nuke the old one
   local previous=$(p6df::modules::lua::luaenv::latest::installed)
-  luaenv uninstall -f $previous
+  luaenv uninstall -f "$previous"
 
   # get the shiny one
   local latest=$(p6df::modules::lua::luaenv::latest)
-  luaenv install -s $latest
+  luaenv install -s "$latest"
 
-  luaenv global $latest
+  luaenv global "$latest"
   luaenv rehash
 
   p6_return_void
@@ -81,17 +81,47 @@ p6df::modules::lua::langs() {
 ######################################################################
 #<
 #
-# Function: str str = p6df::modules::lua::env::prompt::info()
+# Function: str str = p6df::modules::lua::prompt::env()
 #
 #  Returns:
 #	str - str
 #
-#  Environment:	 LUAENV_ROOT
 #>
 ######################################################################
-p6df::modules::lua::env::prompt::info() {
+p6df::modules::lua::prompt::env() {
 
-  local str="lua_root:\t  $LUAENV_ROOT"
+#  local str="lua_root:\t  $LUAENV_ROOT"
+  local str=""
 
   p6_return_str "$str"
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::lua::prompt::lang()
+#
+#>
+######################################################################
+p6df::modules::lua::prompt::lang() {
+
+  local ver
+
+  local ver_mgr
+  ver_mgr=$(luaenv version-name 2>/dev/null)
+  if p6_string_eq "$ver_mgr" "system"; then
+    local ver_sys="sys@"
+    local v
+    v=$(lua  -v | awk '{print $2}')
+    if p6_string_blank "$v"; then
+      ver_sys="sys:no"
+    fi
+    ver="$ver_sys"
+  else
+    ver="$ver_mgr"
+  fi
+
+  local str="lua:$ver"
+
+  p6_return "$str"
 }
